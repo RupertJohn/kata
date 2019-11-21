5f7c3455620b0676f51a0f6307ee84e62d64a0f9
Rupert Saxton <rupertsaxton@gmail.com>
2019-11-21 16:48:24 -0500

    Make it run like a café
    I built the objects but I never actually implemented them. I had 15
    minutes to try this at the end of the day so I pushed to do it in the
    easiest way possible. I had some tricks with the initialization values
    where I set them to values that would work as opposed to making the code
    properly robust

--- 06/cafe.rb
+++ 06/cafe.rb
@@ -10,8 +10,8 @@ class Cafe
     @orders = []
   end
 
-  def coffees_to_make
-    @orders.sum { |order| order.coffees_remaining }
+  def coffees_to_make?
+    orders.length > 0
   end
 
   def new_order
@@ -19,11 +19,11 @@ class Cafe
   end
 
   def coffee_made
-    return if self.coffees_to_make == 0
+    return unless self.coffees_to_make?
     @orders.first.quantity_received += 1
     puts "Coffee up!"
     if @orders.first.complete?
-      @orders.shift 
+      @orders.shift
       puts "Order complete!"
     end
   end
@@ -34,7 +34,7 @@ class CoffeeOrder
   attr_accessor :quantity_received
 
   def initialize
-    @quantity_ordered = (0..5).to_a.sample
+    @quantity_ordered = (1..5).to_a.sample
     puts "#{@quantity_ordered} coffees ordered"
     @quantity_received = 0
   end
@@ -47,3 +47,25 @@ class CoffeeOrder
     self.quantity_ordered == self.quantity_received
   end
 end
+
+minutes = 8 * 60
+coffee_making_time = 3
+mission = Cafe.new
+timeout = (10*rand()).floor
+coffee_timeout = 11
+
+minutes.times do |minute|
+  if timeout < 1
+    timeout = (10*rand()).floor
+    mission.new_order
+  end
+
+  coffee_timeout = minute if mission.coffees_to_make?
+
+  if (minute - coffee_timeout) % coffee_making_time == 0
+    mission.coffee_made
+  end
+  timeout -= 1
+end
+
+puts mission.orders.sum { |order| order.coffees_remaining }
