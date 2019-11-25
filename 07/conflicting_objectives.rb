f32fd0ce07fe653b34be3e448bd830b4dc7d8687
David R. Myers <davidrmyersii@gmail.com>
2019-11-25 10:33:22 -0500

    Group words by length

--- 07/conflicting_objectives.rb
+++ 07/conflicting_objectives.rb
@@ -1,7 +1,11 @@
 class ConflictingObjectives
-  attr_accessor :words
+  attr_accessor :grouped_words, :words
 
   def initialize(file)
     @words = file.read.scan(/^.{1,6}$/)
+  end
+
+  def grouped_words
+    @grouped_words ||= words.group_by(&:length)
   end
 end
