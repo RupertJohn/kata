675078d76301b3944c6c627f727c11dabe606626
David R. Myers <davidrmyersii@gmail.com>
2019-11-25 10:00:36 -0500

    Add functionality for #read_words

--- 07/conflicting_objectives.rb
+++ 07/conflicting_objectives.rb
@@ -1,2 +1,13 @@
 class ConflictingObjectives
+  def initialize(file)
+    @file = file
+  end
+
+  def read_words
+    @words = file.read
+  end
+
+  private
+
+  attr_reader :file, :words
 end
--- 07/spec/conflicting_objectives_spec.rb
+++ 07/spec/conflicting_objectives_spec.rb
@@ -1,13 +1,34 @@
 require_relative '../conflicting_objectives'
 
 RSpec.describe ConflictingObjectives do
-  subject { described_class.new }
+  subject { described_class.new(fake_file) }
 
-  let(:fake_file) { StringIO.new("Here's some text\n and some more") }
-  
+  let(:fake_file) { StringIO.new("Here's\nsome\ntext\nand\nsome\nmore\nreallylongword") }
+
   describe '#read_words' do
     it 'returns a string' do
       expect(subject.read_words).to be_a String
+    end
+
+    it 'returns the file contents' do
+      expect(subject.read_words).to eq("Here's\nsome\ntext\nand\nsome\nmore\nreallylongword")
+    end
+  end
+
+  describe '#filter_words' do
+    before do
+      subject.read_words
+    end
+
+    it 'returns all words that are 6 characters or less' do
+      expect(subject.filter_words).to eq(%w[
+        Here's
+        some
+        text
+        and
+        some
+        more
+      ])
     end
   end
 end
